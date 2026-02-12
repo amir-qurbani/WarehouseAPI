96fbaca0d90e4ea47e68e73e3b70bd5d966c6455
99amiqur <99amiqur@gafe.molndal.se>
2026-02-12 11:39:26 +0100

    Added delete method

--- Warehouse.Api/Controllers/ProductsController.cs
+++ Warehouse.Api/Controllers/ProductsController.cs
@@ -63,6 +63,19 @@ namespace WarehouseAPI.Controllers
             }
             return NoContent();
         }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
 
 
     }
